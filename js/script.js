9815b6cc2bc50414a2efb84ad979e9fb44fff5ca
Max <Maksim.klisov@ukr.net>
2021-02-20 21:29:27 +0200

    test
    test

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-let number = 5;
-const leftBorderWidth = 1;
+'use strict';
+
+let numberOfMovies = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+
+let personalMovieDB = {
+    count : numberOfMovies,
+    movies: {},
+    actors : {},
+    genres : [],
+    privat : false
+}
+
+let a = prompt ('Один из последних просмотренных фильмов?')
+let b = prompt ('На сколько оцените его?')
+let c = prompt ('Один из последних просмотренных фильмов?')
+let d = prompt ('На сколько оцените его?')
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
